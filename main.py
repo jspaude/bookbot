e09a19dbb7f0bc55edb4740933f6e3cb0f896f97
jspaude <justinspaude@gmail.com>
2025-01-27 15:47:25 -0500

    stuck on report

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def main():
     text = contents(file_path)
     count = wordcount(text)
     lower_case = case_conversion(count)
-    letter_count = dictionary_count(lower_case)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
+    #letter_count = dictionary_count(lower_case)
+    #print(letter_count)
     
 def contents(file_path):    
     with open(file_path) as f:
@@ -23,14 +26,28 @@ def case_conversion(count):
         lowered_list.append(lowered_string)
     return(lowered_list)
     
-
+'''
 def dictionary_count(lower_case):
-    letters = {}
+    alphabet =  {
+    'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+    'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+    'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0,' ': 0 }
+
     for l in lower_case:
         for l in l:
-            #letters[l] : 0
-            print(type(l))
-        
+            if l in alphabet:
+                alphabet[l] +=1  
+    return alphabet
+'''
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 main()
 '''
